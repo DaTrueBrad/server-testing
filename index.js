515a3c60c0c4829a98281a4f0da4f332689592d2
Brady Bott <bottbrady@gmail.com>
2021-09-30 08:29:33 -0600

    moved json

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
 
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
+
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
